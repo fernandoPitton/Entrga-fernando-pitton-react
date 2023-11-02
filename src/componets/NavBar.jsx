078ce39b2d6acbb2ab42994432cd76db44e9a831
fernando <ferox_callejeros@hotmail.com>
2023-11-02 20:41:01 -0300

    navbar con react

---
+++ src/componets/NavBar.jsx
@@ -0,0 +1,13 @@
+export default function NavBar(){
+return(
+    <nav>
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">Carrito</a></li>
+            </ul>
+                      
+            
+    </nav>
+)
+}
